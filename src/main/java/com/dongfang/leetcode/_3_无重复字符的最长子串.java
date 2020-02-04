69ba4357284a1086c0407d87235ec32266c5a871
lidf077 <lidf077@gmail.com>
2020-02-04 23:35:19 +0800

    最长子串，不重复

--- src/main/java/com/dongfang/leetcode/_3_无重复字符的最长子串.java
+++ src/main/java/com/dongfang/leetcode/_3_无重复字符的最长子串.java
@@ -24,10 +24,22 @@ package com.dongfang.leetcode;//给定一个字符串，请你找出其中不含
 // Related Topics 哈希表 双指针 字符串 Sliding Window
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class _3_无重复字符的最长子串 {
     public int lengthOfLongestSubstring(String s) {
-        return -1;
+        int res = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int start = 0, end = 0; end < s.length(); end++) {
+            if (map.containsKey(s.charAt(end))) {
+                start = Math.max(map.get(s.charAt(end)), start);
+            }
+            res = Math.max(res, (end - start + 1));
+            map.put(s.charAt(end), end + 1);
+        }
+        return res;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
