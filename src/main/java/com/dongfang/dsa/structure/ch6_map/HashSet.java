1fcf50b9b41e636a94e74639c512770e702ead0c
lidf077 <lidf077@gmail.com>
2020-02-17 22:11:23 +0800

    哈希表实现的map与set

---
+++ src/main/java/com/dongfang/dsa/structure/ch6_map/HashSet.java
@@ -0,0 +1,49 @@
+package com.dongfang.dsa.structure.ch6_map;
+
+
+import com.dongfang.dsa.structure.Visitor;
+import com.dongfang.dsa.structure.ch5_set.Set;
+
+public class HashSet<E> implements Set<E> {
+    private HashMap<E, Object> map = new HashMap<>();
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public boolean contains(E element) {
+        return map.containsKey(element);
+    }
+
+    @Override
+    public void add(E element) {
+        map.put(element, null);
+    }
+
+    @Override
+    public void remove(E element) {
+        map.remove(element);
+    }
+
+    @Override
+    public void traversal(Visitor<E> visitor) {
+        map.traversal(new Map.Visitor<E, Object>() {
+            public boolean visit(E key, Object value) {
+                return visitor.visit(key);
+            }
+        });
+    }
+
+}
---
+++ src/main/java/com/dongfang/dsa/structure/ch6_map/LinkedHashSet.java
@@ -0,0 +1,48 @@
+package com.dongfang.dsa.structure.ch6_map;
+
+import com.dongfang.dsa.structure.Visitor;
+import com.dongfang.dsa.structure.ch5_set.Set;
+
+public class LinkedHashSet<E> implements Set<E> {
+    private LinkedHashMap<E, Object> map = new LinkedHashMap<>();
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public boolean contains(E element) {
+        return map.containsKey(element);
+    }
+
+    @Override
+    public void add(E element) {
+        map.put(element, null);
+    }
+
+    @Override
+    public void remove(E element) {
+        map.remove(element);
+    }
+
+    @Override
+    public void traversal(Visitor<E> visitor) {
+        map.traversal(new Map.Visitor<E, Object>() {
+            public boolean visit(E key, Object value) {
+                return visitor.visit(key);
+            }
+        });
+    }
+
+}
