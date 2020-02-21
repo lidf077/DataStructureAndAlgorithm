45fc22df48c9dbb42d255bc657bc7a671ccba527
lidf077 <lidf077@gmail.com>
2020-02-21 18:29:40 +0800

    插入排序的优化

--- src/main/java/com/dongfang/dsa/algorithm/sort/InsertionSort.java
+++ src/main/java/com/dongfang/dsa/algorithm/sort/InsertionSort.java
@@ -36,4 +36,68 @@ public class InsertionSort<E extends Comparable<E>> extends Sort<E> {
      *      4、当逆序对的数量极少时，插入排序的效率特别高，速度甚至比O(nlogn)的快速排序还要快
      *      5、数据量不是特别大的时候，插入排序的效率也是非常好的
      */
+
+
+
+    private void insertionSortOpt() {
+        int len = array.length;
+        for (int begin = 1; begin < len; begin++) {
+            int cur = begin;
+            E value = array[cur];
+            // 将元素向后面挪动，value暂存起来，不要一直要cur指针上的值交换，减少交换的三次操作
+            for (; cur > 0 && compare(value, array[cur - 1]) < 0; cur--) {
+                array[cur] = array[cur - 1];
+            }
+            array[cur] = value;
+        }
+    }
+
+    /**
+     * 二分搜索优化
+     * 在元素value插入的过程中，可以二分搜索出合适的插入位置，然后再将元素v插入
+     * 挪动是一样的，但是二分搜索优化了比较次数
+     */
+
+    private void insertionSortOptByBinarySearch() {
+        int len = array.length;
+        for (int begin = 1; begin < len; begin++) {
+            insert(begin, search(begin));
+        }
+    }
+
+    /**
+     * 将source位置的元素插入到dest位置
+     * @param source
+     * @param dest
+     */
+    private void insert(int source, int dest) {
+        E target = array[source];
+        // 将[insertIndex, begin)的向右挪动一个单位，大的先挪动，--
+        for (int i = source; i > dest ; i--) {
+            array[i] = array[i - 1];
+        }
+        array[dest] = target;
+    }
+
+    /**
+     * 利用二分搜索找到 index 位置元素的待插入位置
+     * 已经排好序的数组区间 [0, index)
+     *
+     * @param index
+     * @return
+     */
+    private int search(int index) {
+        E target = array[index];
+        int begin = 0;
+        int end = index;
+        while (begin < end) {
+            int mid = (begin + end) >> 1;
+            if (compare(target, array[mid]) < 0) {
+                end = mid;
+            } else if (compare(target, array[mid]) >= 0) {
+                begin = mid + 1;
+            }
+        }
+        return begin;
+    }
 }
---
+++ src/main/java/com/dongfang/dsa/algorithm/sort/ShellSort.java
@@ -0,0 +1,12 @@
+package com.dongfang.dsa.algorithm.sort;
+
+public class ShellSort<E extends Comparable<E>> extends Sort<E> {
+    @Override
+    protected void sort() {
+
+    }
+
+    private void shellSort() {
+
+    }
+}
