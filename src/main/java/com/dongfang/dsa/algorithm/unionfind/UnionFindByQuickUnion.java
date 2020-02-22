9d522fcf8d1496cfd3e84a3fa7a1fe5b2134ff64
lidf077 <lidf077@gmail.com>
2020-02-22 08:48:57 +0800

    使用quick union 实现并查集

---
+++ src/main/java/com/dongfang/dsa/algorithm/unionfind/UnionFindByQuickUnion.java
@@ -0,0 +1,17 @@
+package com.dongfang.dsa.algorithm.unionfind;
+
+public class UnionFindByQuickUnion extends UnionFind {
+    public UnionFindByQuickUnion(int capacity) {
+        super(capacity);
+    }
+
+    @Override
+    public int find(int v) {
+        return 0;
+    }
+
+    @Override
+    public void union(int v1, int v2) {
+
+    }
+}
