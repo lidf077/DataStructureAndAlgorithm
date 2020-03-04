4f13eb325f4bbc3c96850bd226f35c66f8d538bf
lidf077 <lidf077@gmail.com>
2020-03-04 22:09:44 +0800

    添加测试数据

--- src/main/java/com/dongfang/dsa/algorithm/graph/GraphDemo.java
+++ src/main/java/com/dongfang/dsa/algorithm/graph/GraphDemo.java
@@ -15,7 +15,46 @@ public class GraphDemo {
         graph.addEdge("V3", "V4", 1);
         graph.addEdge("V0", "V4", 6);
         ((ListGraph) graph).print();
+    }
 
+    /**
+     * 有向图
+     */
+    private Graph<Object, Double> directedGraph(Object[][] data) {
+        Graph<Object, Double> graph = new ListGraph<>();
+        for (Object[] edge : data) {
+            if (edge.length == 1) {
+                graph.addVertex(edge[0]);
+            } else if (edge.length == 2) {
+                graph.addEdge(edge[0], edge[1]);
+            } else if (edge.length == 3) {
+                double weight = Double.parseDouble(edge[2].toString());
+                graph.addEdge(edge[0], edge[1], weight);
+            }
+        }
+        return graph;
+    }
 
+    /**
+     * 无向图
+     *
+     * @param data
+     * @return
+     */
+    private Graph<Object, Double> undirectedGraph(Object[][] data) {
+        Graph<Object, Double> graph = new ListGraph<>();
+        for (Object[] edge : data) {
+            if (edge.length == 1) {
+                graph.addVertex(edge[0]);
+            } else if (edge.length == 2) {
+                graph.addEdge(edge[0], edge[1]);
+                graph.addEdge(edge[1], edge[0]);
+            } else if (edge.length == 3) {
+                double weight = Double.parseDouble(edge[2].toString());
+                graph.addEdge(edge[0], edge[1], weight);
+                graph.addEdge(edge[1], edge[0], weight);
+            }
+        }
+        return graph;
     }
 }
